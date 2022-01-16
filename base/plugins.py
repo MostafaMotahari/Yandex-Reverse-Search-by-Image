4044d9b8cc1433aa461aecbb30460d476816cc51
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-01-16 16:44:30 -0500

    The main change. Change the search engine from Google image search to Yandex service

--- base/plugins.py
+++ base/plugins.py
@@ -36,47 +36,34 @@ def google_search(file_path, message):
 
     # Parse all needed information
     b = bs4.BeautifulSoup(response.text, "html.parser")
-    open("test.html", "w").write(response.text)
+
     # Try parse suggestion
     try:
         find_sug = b.select('.CbirSites-Item')
-        print(find_sug)
-        suggestion = find_sug[0].text
+        titles = []
+        counter = 0
+
+        for title in find_sug:
+            titles.append(title.text[7:].split("\n")[0])
+
+            counter += 1
+
+            if counter == 5:
+                break
+
+        suggestion = "\n\n".join(titles)
+
     except Exception as e:
         print(e, "No suggestion")
         suggestion = False
 
-    # Try parse similar
-    try:
-        find_similar = b.select(".e2BEnf")
-        similar = 'https://www.google.com' + str(find_similar[0].a['href'])
-    except Exception as e:
-        print(e, "No similar")
-        similar = False
-
-    # Try parse sites
-    try:
-        find_sites = b.find_all('div', {'class': 'yuRUbf'})
-        sites = [(si.a['href'], si.h3.text) for si in find_sites]
-    except Exception as e:
-        print(e, "No sites")
-        sites = False
-
     # Send
     txt = ''
     if suggestion:
-        txt = '<b>Main suggestion: %s</b>\n\n' % escape(suggestion)
+        txt = '<b>Main suggestion: \n\n%s</b>\n\n' % escape(suggestion)
 
-    txt += '<b>Search results:</b>\n\n'
-    if sites:
-        txt += '\n\n'.join([f'<a href="{escape(site[0])}">{escape(site[1])}</a>' for site in sites])
 
     inline_keyboard = []
-    if similar:
-        inline_keyboard.append([
-            InlineKeyboardButton(text="🔗 Link to similar images", url=similar)
-        ])
-
     inline_keyboard.append([
         InlineKeyboardButton(text="🌐 Search page", url="https://www.google.com/searchbyimage?image_url=" + img_url)
     ])
